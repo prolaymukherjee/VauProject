871b3c8a90fc1f5f68e58f9b787a42e8897487c1
prolay <prolaycsediu@gmail.com>
2018-11-27 01:33:10 +0600

    first commit

---
+++ ecommerce/src/components/pages/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login">
+    <div class="loginHeader">
+      user login
+    </div>
+       
+     <div class="loginContainer">
+        <table>
+          <tr>
+            <td>username</td>
+            <td>:</td>
+            <td><input type="text" placeholder="username" ></td>
+          </tr>
+
+          <tr>
+            <td>password</td>
+            <td>:</td>
+            <td><input type="password" placeholder="password" ></td>
+          </tr>
+
+          <tr>
+            <td></td>
+            <td></td>
+            <td><button class="addBtn">Login</button></td>
+          </tr>
+
+        </table>
+     </div>  
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
